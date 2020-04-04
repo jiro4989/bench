6441a3e0a1292edb37a37ced8ab11ee158413aa4
jiro4989 <jiroron666@gmail.com>
2020-04-04 18:10:47 +0900

    fix script

--- cases/case_001/main.nim
+++ cases/case_001/main.nim
@@ -1,27 +1,8 @@
-import os, threadpool
+echo NimVersion
 
 var s: string
+for i in 1..1000000:
+  s.add($i)
 
-proc heavyMethod1(): int =
-    echo "1: Start"
-    sleep(5000)
-    echo "1: End"
-    {.gcsafe.}:
-      s.add("m1")
-    return 5
-
-proc heavyMethod2(): int =
-    echo "2: Start"
-    sleep(2000)
-    echo "2: End"
-    {.gcsafe.}:
-      s.add("m2")
-    return 20
-
-var result1 = spawn heavyMethod1()
-var result2 = spawn heavyMethod2()
-sync()
-
-echo ^result1
-echo ^result2
-echo s
+writeFile("out.txt", s)
+echo "end"
