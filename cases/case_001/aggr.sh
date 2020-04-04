fbd0abd7febb7a1ece17e31e94bcb987983a5e27
jiro4989 <jiroron666@gmail.com>
2020-04-04 18:48:26 +0900

    fix table format

--- cases/case_001/aggr.sh
+++ cases/case_001/aggr.sh
@@ -3,5 +3,8 @@
 set -eu
 
 for l in go nim_1.0.2 nim_1.2.0 nim_1.2.0_arc python; do
-  cat result/$l/*.txt | grep real | sed 's/0m//g;s/s//g' | awk -v l=$l '{ sum += $2 } END{ print l, "average:", sum / NR }'
-done | column -t
+  cat result/$l/*.txt |
+    grep real |
+    sed 's/0m//g;s/s//g' |
+    awk -v l=$l '{ sum += $2 } END{ print "|", l, "|", sum / NR, "|" }'
+done | awk 'BEGIN{ print "|language|time (sec)|"; print "|--:|:--|" } {print $0}'
